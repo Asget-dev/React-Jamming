ee6597201c1dbe159dcf26989b7f56c0ffde7f22
Asget Dev <asgedomehaile@gmail.com>
2020-05-18 20:51:38 -0700

    Added React Jamming Project

---
+++ src/util/Spotify.js
@@ -0,0 +1,49 @@
+
+let accessToken;
+
+const Spotify = {
+    get(accessToken){
+        if (accessToken){
+            return accessToken;
+        }
+
+        //check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = expiresInMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            //This clears the parameters, allowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }else{
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+            window.location = accessUrl;
+        }
+    },
+    search(term){
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${TERM}`,{
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response=>{
+            return response.jason();
+        }).then(jsonResponse => {
+            if(!jsonResponse.tracks){
+                return [];
+            }
+            return jsonResponse.tracks.items.map(track=>({
+                id: track.id,
+                name: track.name,
+                artist: track.artist[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+        });
+    },
+}
+
+export default Spotify;
